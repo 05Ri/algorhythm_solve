e5953a608eed73326b287a8f05148993eb5eec8a
5Ri <yh980827@naver.com>
2025-10-27 01:26:25 +0900

    [BOJ] 10699 오늘 날짜

---
+++ src/baekjoon/BOJ_10699_오늘_날짜/Main.java
@@ -0,0 +1,11 @@
+package baekjoon.BOJ_10699_오늘_날짜;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main (String[] args) {
+        LocalDate now = LocalDate.now();
+
+        System.out.println(now);
+    }
+}
