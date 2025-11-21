e0b099f968b689f84d259f0ccbd2a968310c1c6d
5Ri <yh980827@naver.com>
2025-11-21 12:59:11 +0900

    [BOJ] 11718 그대로 출력하기

---
+++ src/baekjoon/BOJ_11718_그대로_출력하기/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.BOJ_11718_그대로_출력하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+    }
+}
