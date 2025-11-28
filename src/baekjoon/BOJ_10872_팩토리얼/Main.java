bc77444ae40b96650aba390c4ad730506a7f68d5
5Ri <yh980827@naver.com>
2025-11-28 19:19:44 +0900

    [BOJ] 10872 팩토리얼

---
+++ src/baekjoon/BOJ_10872_팩토리얼/Main.java
@@ -0,0 +1,23 @@
+package baekjoon.BOJ_10872_팩토리얼;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long result = 1;
+
+        if (N >= 2) {
+            for (int i = 2; i <= N; i++) {
+                result *= i;
+            }
+        }
+        
+        System.out.println(result);
+    }
+}
