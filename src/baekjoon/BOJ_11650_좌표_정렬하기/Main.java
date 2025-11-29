f5989dc0898498e897bbab71fac51567221517bd
5Ri <yh980827@naver.com>
2025-11-30 08:32:34 +0900

    [BOJ] 11650 좌표 정렬하기

---
+++ src/baekjoon/BOJ_11650_좌표_정렬하기/Main.java
@@ -0,0 +1,41 @@
+package baekjoon.BOJ_11650_좌표_정렬하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] axis = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            axis[i][0] = x;
+            axis[i][1] = y;
+        }
+
+        Arrays.sort(axis, (a, b) -> {
+            if (a[0] != b[0]) {
+                return Integer.compare(a[0], b[0]);
+            }
+            return Integer.compare(a[1], b[1]);
+        });
+
+        for (int i = 0; i < N; i++) {
+            sb.append(axis[i][0]).append(" ").append(axis[i][1]).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
