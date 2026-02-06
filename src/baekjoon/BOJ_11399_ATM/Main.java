781c79e984a7291b29194ae6fdd3e8e76f64b697
5Ri <yh980827@naver.com>
2026-02-06 21:41:49 +0900

    [BOJ] 11399 ATM

---
+++ src/baekjoon/BOJ_11399_ATM/Main.java
@@ -0,0 +1,33 @@
+package baekjoon.BOJ_11399_ATM;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] times = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            times[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(times);
+
+        int sum = 0;
+        int result = 0;
+
+        for (int i = 0; i < N; i++) {
+            sum += times[i];
+            result += sum;
+        }
+
+        System.out.println(result);
+    }
+}
